90a1ab6203ae7c83b229b61d485737ca9f1c2515
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2021-09-21 21:40:19 +0700

    seeders

--- app/Models/CampBenefit.php
+++ app/Models/CampBenefit.php
@@ -9,5 +9,5 @@ class CampBenefit extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name'];
+    protected $fillable = ['camp_id', 'name'];
 }
---
+++ database/seeders/CampTableSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Camp;
+
+class CampTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $camps = [
+            [
+                'title' => 'Gila Belajar',
+                'slug' => 'gila-belajar',
+                'price' => 280,
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time()),
+            ],
+            [
+                'title' => 'Baru Mulai',
+                'slug' => 'baru-mulai',
+                'price' => 140,
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time()),
+            ],
+        ];
+
+        // 1st method
+        // foreach ($camps as $key => $camp) {
+        //     Camp::create($camp);
+        // }
+
+        // 2nd method
+        Camp::insert($camps);
+    }
+}
